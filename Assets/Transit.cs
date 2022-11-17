5487f5b22392b15b94eeebd39ec36b500e99b613
Hai The Dude <hainghiem375@gmail.com>
2022-11-17 00:29:41 -0500

    working transit system

--- Assets/Transit.cs
+++ Assets/Transit.cs
@@ -8,13 +8,13 @@ public class Transit : MonoBehaviour
     [field: SerializeField] public float CheckpointWaitTime { get; private set; }
     [field: SerializeField] public float CurrentCheckpointWaitTime { get; private set; }
     [field: SerializeField] public float Speed { get; private set; }
-    [field: SerializeField] public GameObject VehicleModel { get; private set; }
-    [field: SerializeField] public bool CanBoard { get; private set; }
+    [field: SerializeField] public GameObject Vehicle { get; private set; }
+    [field: SerializeField] public bool Boarding { get; private set; }
     [field: SerializeField] public bool CanStart { get; private set; }
     [field: SerializeField] public GameObject CurrentCheckpoint { get; private set; }
+    [field: SerializeField] public GameObject NextCheckpoint { get; private set; }
+    [field: SerializeField] public float DistanceFromNextCheckpoint { get; private set; }
 
-
-    // Start is called before the first frame update
     void Start()
     {
 
@@ -25,41 +25,33 @@ public class Transit : MonoBehaviour
         if (CurrentCheckpoint == null)
         {
             CurrentCheckpoint = Checkpoints[0];
+            Boarding = true;
+            Vehicle.transform.position = CurrentCheckpoint.transform.position;
         }
 
-        CurrentCheckpointWaitTime = CheckpointWaitTime;
-
+        if (CurrentCheckpointWaitTime == 0)
+        {
+            CurrentCheckpointWaitTime = CheckpointWaitTime;
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
         ActivateTransit();
     }
 
-    private GameObject TravelToNextCheckpoint(bool canStart, List<GameObject> checkpoints, GameObject currentCheckpoint, GameObject VehicleModel)
+    private float GetDistanceFromCheckpoint(GameObject checkpoint, GameObject Vehicle)
     {
-        GameObject nextCheckpoint = GetNextCheckpoint(currentCheckpoint, checkpoints);
-
-        float distance = Vector2.Distance(currentCheckpoint.transform.position, nextCheckpoint.transform.position);
-
-        // Move the Vehicle GameObject based on its speed, deltaTime, and distance between checkpoint
-
-        // when arrive at a checkpoint
-
-        // wait and emit Unity Event
-        BroadcastAllAboard(nextCheckpoint);
-
-        // flip boolean CanBoard so player can board
-
-        return nextCheckpoint;
+        float distance = Vector2.Distance(Vehicle.transform.position, checkpoint.transform.position);
+        BroadcastAllAboard(checkpoint);
+        return distance;
 
     }
 
     private GameObject GetNextCheckpoint(GameObject current, List<GameObject> checkpoints)
     {
         int currentCheckpointIndex = checkpoints.IndexOf(current);
-        if (currentCheckpointIndex != checkpoints.Count - 1)
+        if (currentCheckpointIndex == checkpoints.Count - 1)
         {
             return checkpoints[0];
         }
@@ -68,20 +60,39 @@ public class Transit : MonoBehaviour
 
     private void ActivateTransit()
     {
-        if (CanBoard)
+        if (Boarding)
+
         {
+            Vehicle.transform.position = CurrentCheckpoint.transform.position;
             if (CurrentCheckpointWaitTime > 0)
             {
                 CurrentCheckpointWaitTime -= Time.deltaTime;
-                Debug.Log(CurrentCheckpointWaitTime);
+                Debug.Log("Currently at a checkpoint for: " + CurrentCheckpointWaitTime);
             }
 
-            else
+            if (CurrentCheckpointWaitTime < 0)
             {
-                TravelToNextCheckpoint(CanStart, Checkpoints, CurrentCheckpoint, VehicleModel);
+                NextCheckpoint = GetNextCheckpoint(CurrentCheckpoint, Checkpoints);
+                DistanceFromNextCheckpoint = GetDistanceFromCheckpoint(NextCheckpoint, Vehicle);
                 CurrentCheckpointWaitTime = CheckpointWaitTime;
-                CanBoard = false;
+                Boarding = false;
             }
+
+        }
+        if (!Boarding)
+        {
+            if (DistanceFromNextCheckpoint > 0)
+            {
+                DistanceFromNextCheckpoint -= Time.deltaTime;
+                Debug.Log("Remaining distance from next checkpoint: " + DistanceFromNextCheckpoint);
+            }
+
+            if (DistanceFromNextCheckpoint < 0)
+            {
+                CurrentCheckpoint = NextCheckpoint;
+                Boarding = true;
+            }
+
         }
     }
 
